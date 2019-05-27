2b9e1d7312c7fafb3a256f7f4e3832b1ddc777d5
xxy <Rsrnl_Wysxr@163.com>
2019-05-28 00:10:15 +0800

    add sign in

--- pages/course/course.js
+++ pages/course/course.js
@@ -12,19 +12,20 @@ Page({
     hasQiandao: false,
     teacher: "王红",
     course: "数字信号处理",
+    course_id: 0,
   },
-
-  qiandao: function () {
+  qiandao: function() {
     if (!this.data.showQiandao && !this.data.hasQiandao) {
       this.setData({
         showQiandao: true,
       });
     } else {
+      this.getSignin();
       if (!this.data.hasQiandao) {
         wx.showLoading({
           title: '签到中',
-        })
-        setTimeout(function () {
+        });
+        setTimeout(function() {
           wx.hideLoading();
           wx.showToast({
             title: '签到成功'
@@ -43,7 +44,7 @@ Page({
     }
   },
 
-  jianyan: function () {
+  jianyan: function() {
     if (!this.data.hasQiandao) {
       wx.showToast({
         title: '请先进行签到',
@@ -52,9 +53,9 @@ Page({
     } else {
       wx.chooseImage({
         success(res) {
-            wx.showToast({
-              title: '上传成功'
-            })
+          wx.showToast({
+            title: '上传成功'
+          })
         }
       })
     }
@@ -63,7 +64,7 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
       student: app.globalData.name
     });
@@ -72,49 +73,94 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  onShow: function() {
+    this.getSource();
   },
-
+  getSource: function() {
+    var that = this;
+    var course_url = app.globalData.urlBase + "/course";
+    var account = app.globalData.account;
+    wx.request({
+      url: course_url,
+      data: {
+        account: account
+      },
+      header: {
+        "content-type": "json"
+      },
+      method: 'POST',
+      dataType: 'json',
+      success: function(res) {
+        res = res.data;
+        that.setData({
+          teacher: res.teacher_name,
+          course: res.course_name,
+          course_id: res.course_id
+        });
+      }
+    });
+  },
+  getSignin: function() {
+    var that = this;
+    var account = app.globalData.account;
+    var name = app.globalData.name;
+    var signin_url = app.globalData.urlBase + "/signin";
+    wx.request({
+      url: signin_url,
+      data: {
+        course_id: that.data.course_id,
+        account: account,
+        student_name: name,
+        course_name:that.data.course
+      },
+      header: {
+        "content-type": "json"
+      },
+      method: 'POST',
+      dataType: 'json',
+      success: function(res) {
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
